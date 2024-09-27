b10318c15f2f6655017efab9d04f13ec4e9bc117
louise <louise.millard@gmail.com>
2024-09-27 11:51:12 +0100

    fix for accel checking - look at original not derived

--- week3-linux/1-data-prep/2-data-check-accel.sh
+++ week3-linux/1-data-prep/2-data-check-accel.sh
@@ -8,25 +8,30 @@ DATADIR='../../data'
 # pick one example and check the top
 
 # list some files
+echo "Some file names:"
 ls ${DATADIR}/original/accel/ | head
 
+# look at the top of an example file
+echo "Top of example file:"
 head ${DATADIR}/original/accel/accel-31128.txt
 
 # print top of example accel file
+echo "Number of fields for top of example file:"
 awk -F'\t' '{print NF}' ${DATADIR}/original/accel/accel-31128.txt | head
 
 
 
-#### check that all lines in all the accel files (except first) have 8 columns and fix those that don't
+#### check that all lines in all the accel files (except first) have 8 columns
 
 # check that all lines have 8 columns
-cat ${DATADIR}/derived/accel/accel-*.txt | grep -v '<' | awk -F'\t' '{print NF}' | grep -v 8 | sort -u
+echo "Number of columns found in the different accelerometer files:"
+cat ${DATADIR}/original/accel/accel-*.txt | grep -v '<' | awk -F'\t' '{print NF}' | grep -v 8 | sort -u
 
 # print all lines that don't have 8 columns
-cat ${DATADIR}/derived/accel/accel-*.txt | grep -v '<' | awk -F'\t' '(NF!=8){print $0}' | sort -u
+echo "Lines that don't have 8 columns:"
+cat ${DATADIR}/original/accel/accel-*.txt | grep -v '<' | awk -F'\t' '(NF!=8){print $0}' | sort -u
 
 # see which files the NA lines exist in
-grep -vH '<' ${DATADIR}/derived/accel/accel-*.txt | grep "NA\tNA\tNA" | sort -u
-
-#awk -F'\t' '(NF!=8){print $0}' ${DATADIR}/derived/accel/accel-*.txt | sort -u
+echo "Files containing NA lines:"
+grep -vH '<' ${DATADIR}/original/accel/accel-*.txt | grep "NA\tNA\tNA" | sort -u
 
